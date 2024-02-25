134832d4625568ac7e7762369dafbc006729d804
Hwang gend <80672346+sernan96@users.noreply.github.com>
2024-02-25 21:13:57 +0900

    [ADD] main.dart

---
+++ Front/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:haksikgo/pages/home_page.dart';
+import 'package:haksikgo/pages/login_page.dart';
+
+void main() {
+  runApp(const HomeApp());
+}
+
+class HomeApp extends StatelessWidget {
+  const HomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: const Color(0xFF376860),
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Color(0xFF376860),
+          selectionColor: Color(0xFF376860),
+          selectionHandleColor: Color(0xFF376860),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFF376860)),
+          ),
+          labelStyle: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        colorScheme: ColorScheme.fromSwatch()
+            .copyWith(secondary: const Color(0xFF376860)),
+      ),
+      initialRoute: '/Home_Page', // 초기 화면 설정
+      routes: {
+        '/Home_Page': (context) => const HomePage(), // Home 페이지
+        '/login': (context) => const LoginApp(), // LoginPage로 이동
+      },
+    );
+  }
+}
